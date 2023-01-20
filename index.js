f0fa967fbc7941f86b579694a7975bce453bdba7
QuantumK <quantumk9@hotmail.com>
2023-01-20 18:38:18 +0000

    add two questions for testing

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "Enter title of project:",
+  },
+  {
+    type: "input",
+    message: "Description: ",
+    name: "description",
+  },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
